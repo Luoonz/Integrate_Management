8e1772e2af3aa6782d60e5a77ebe5ca22744cacf
Lamlic36 <luoonz@gmailc.om>
2021-07-30 17:56:41 +0900

    통합 관리앱

---
+++ app/src/main/java/luoonz/self/integrate_management/MainActivity.kt
@@ -0,0 +1,11 @@
+package luoonz.self.integrate_management
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
